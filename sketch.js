78563d3f6a8c1d0730f2d8925e0fd39548672403
jkutkut <jorgeregonzalez@gmail.com>
2021-10-22 13:21:45 +0200

    Now using standard code to represent elements

--- sketch.js
+++ sketch.js
@@ -1,20 +1,67 @@
 var mainCanvas, ctx;
+var game;
 
 var offset = 20;
 
 window.onload = () => {
     mainCanvas = $("mainCanvas");
     ctx = document.getElementById("mainCanvas").getContext('2d');
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "white";
 
     windowResize();
-    $(window).resize(windowResize)
+    $(window).resize(windowResize);
+
+    game = new Spacewar(ctx);
 };
 
 
 function show() {
     ctx.clearRect(0, 0, gateEditorCanvas.width, gateEditorCanvas.height);
 };
+
+const canvas_draw = {
+    line: (startPoint, endPoint) => {
+        ctx.beginPath();
+        ctx.moveTo(...startPoint.pos);
+        ctx.lineTo(...endPoint.pos);
+        ctx.closePath();
+        ctx.stroke();
+    },
+    arc: (x, y, radius, startAngle, endAngle, fill=false) => {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, startAngle, endAngle);
+        if (fill) {
+            ctx.fill();
+        }
+        else {
+            ctx.stroke();
+        }
+    },
+    element: (element, fill=false) => {
+        let shape = element.shape;
+
+        for (let i = 0; i < shape.lines.length; i++) {
+            canvas_draw.line(...shape.lines[i]);
+        }
+        
+        for (let i = 0; i < shape.arcs.length; i++) {
+            canvas_draw.arc(...shape.arcs[i], fill);
+        }
+    },
+    array: (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            canvas_draw.element(arr[i]);
+    
+            for (let j = 0; j < arr[i].IO_SIZE.IN; j++) {
+                canvas_draw.element(pointShape(arr[i].getIN_location(j)), true);
+            }
+    
+            for (let j = 0; j < arr[i].IO_SIZE.OUT; j++) {
+                canvas_draw.element(pointShape(arr[i].getOUT_location(j)), true);
+            }
+        }
+    }
+}
 
 
 function windowResize() {
