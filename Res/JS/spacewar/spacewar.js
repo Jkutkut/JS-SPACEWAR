9cbb97432a5bdac1cb7096b9dd180d49f4b24141
jkutkut <jorgeregonzalez@gmail.com>
2021-10-22 13:21:24 +0200

    Star now can be represented using the spacewar class

--- Res/JS/spacewar/spacewar.js
+++ Res/JS/spacewar/spacewar.js
@@ -1,6 +1,22 @@
 class Spacewar {
     constructor(ctx) {
+        this.ctx = ctx;
+
         this.star = new Star(ctx);
-        this.star.show();
+
+        this.show();
+    }
+
+
+    show() {
+        this.ctx.save(); // save previous styles & set our current styles
+    
+        this.ctx.fillStyle = 'white';
+        this.ctx.lineWidth = 3;
+
+        // Show star
+        canvas_draw.element(this.star, true);
+
+        this.ctx.restore();
     }
 }
--- Res/JS/spacewar/star.js
+++ Res/JS/spacewar/star.js
@@ -5,28 +5,16 @@ class Star {
         this.pos = new Point(ctx.canvas.width >> 1, ctx.canvas.height >> 1);
     }
 
-    show() {
-        this.ctx.save(); // save previous styles & set our current styles
-    
-        this.ctx.fillStyle = 'white';
-        this.ctx.lineWidth = 3;
-
-        this.ctx.beginPath();
-        
+    get shape() {
         let radius = 5;
         let startAngle = 0;
         let endAngle = Math.PI * 2;
-        let fill = true; 
-        
-        this.ctx.arc(...this.pos.pos, radius, startAngle, endAngle);
 
-        if (fill) {
-            this.ctx.fill();
-        }
-        else {
-            this.ctx.stroke();
-        }
-
-        this.ctx.restore();
+        return {
+            lines: [],
+            arcs: [
+                [...this.pos.pos, radius, startAngle, endAngle]
+            ]
+        };
     }
 }
