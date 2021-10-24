e0fb57baf5d8c2768321961839e8e4f088aa8e2a
= <=>
2021-10-24 13:00:09 +0200

    Now ships can be rotated based on controls

--- Res/JS/spacewar/player/spacewarPlayer.js
+++ Res/JS/spacewar/player/spacewarPlayer.js
@@ -1,15 +1,76 @@
+$.getJSON(
+    "Res/JS/spacewar/player/controls.json",
+    (json) => {
+        SpacewarPlayer.CONTROLS = json.controls;
+    }
+);
+
 class SpacewarPlayer {
-    constructor(controls, pos, v) {
-        this.ship = new SpacewarShip(pos, v);
 
-        this.controls = controls;
+    static CONTROLS;
+
+    constructor(index, ship) {
+        this.ship = ship;
+
+        this.controls = SpacewarPlayer.CONTROLS[index];
+
+        this.state = {
+            right: false,
+            left: false,
+            forward: false,
+            shoot: false
+        }
     }
 
-    keyUp(k) {
-
+    update() {
+        // console.log(this.state)
+        let deltaA = 0.02;
+        if (this.state.right) {
+            this.ship.angle += deltaA;
+        }
+        if (this.state.left) {
+            this.ship.angle -= deltaA;
+        }
+        if (this.forward) {
+            // TODO propulsor
+        }
+        if (this.shoot) {
+            // TODO shoot
+            this.shoot = false;
+        }
     }
 
-    keyDown(k) {
-        
+    keyDown(keyCode) {
+        switch(keyCode) {
+            case this.controls.right:
+                this.state.right = true;
+                break;
+            case this.controls.left:
+                this.state.left = true;
+                break;
+            case this.controls.forward:
+                this.state.forward = true;
+                break;
+            case this.controls.shoot:
+                this.state.shoot = true;
+                break;
+        }
+    }
+
+    keyUp(keyCode) {
+        switch(keyCode) {
+            case this.controls.right:
+                this.state.right = false;
+                break;
+            case this.controls.left:
+                this.state.left = false;
+                break;
+            case this.controls.forward:
+                this.state.forward = false;
+                break;
+            case this.controls.shoot:
+                this.state.shoot = false;
+                break;
+        }
     }
 }
