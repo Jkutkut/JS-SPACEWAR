ff9f04293412260c8b83ed34acb2de91c365d01b
= <=>
2021-10-24 11:03:02 +0200

    players will control the ships

---
+++ Res/JS/spacewar/player/spacewarPlayer.js
@@ -0,0 +1,15 @@
+class SpacewarPlayer {
+    constructor(controls, pos, v) {
+        this.ship = new SpacewarShip(pos, v);
+
+        this.controls = controls;
+    }
+
+    keyUp(k) {
+
+    }
+
+    keyDown(k) {
+        
+    }
+}
