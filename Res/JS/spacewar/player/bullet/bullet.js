f7715b501513c238b2bff17af0c332046d9e5f99
jkutkut <jorgeregonzalez@gmail.com>
2021-10-25 08:36:58 +0200

    Basic logic to create a bullet added

--- Res/JS/spacewar/player/bullet/bullet.js
+++ Res/JS/spacewar/player/bullet/bullet.js
@@ -1,15 +1,39 @@
 class Bullet {
     static DEFAULT_SHAPE = {
         shapes: [
-
+            [
+                new Point(-S * 0.1, -S * 0.1),
+                new Point(-S * 0.1, S * 0.1),
+                new Point(S * 0.2, S)
+            ]
         ],
         lines: [],
         arcs: []
     };
 
-    constructor(pos, angle, v) {
+    constructor(pos, v) {
         this.pos = pos;
-        this.angle = angle;
         this.v = v;
+    }
+
+    get shape() {
+        return this._shapeOBJ;
+    }
+
+    update() {
+        this.pos.advanceWithDirection(this.v);
+
+        // Update shapeOBJ
+        this._shapeOBJ = {shapes: [], lines: [], arcs: []};
+
+        for (let i = 0, j; i < Bullet.DEFAULT_SHAPE.shapes.length; i++) {
+            this._shapeOBJ.shapes.push([]);
+
+            for (j = 0; j < Bullet.DEFAULT_SHAPE.shapes[i].length; j++) {
+                this._shapeOBJ.shapes[i].push(Bullet.DEFAULT_SHAPE.shapes[i][j].clone());
+                this._shapeOBJ.shapes[i][j].rotateBy(this.angle);
+                this._shapeOBJ.shapes[i][j].advanceWithDirection(this.pos);
+            }
+        }
     }
 }
