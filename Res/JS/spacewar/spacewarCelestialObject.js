cf759cbc8eb59957fed8dfbbb8e5c60425c70514
jkutkut <jorgeregonzalez@gmail.com>
2021-10-26 10:45:08 +0200

    Workin on class to be the father of all objects

--- Res/JS/spacewar/spacewarCelestialObject.js
+++ Res/JS/spacewar/spacewarCelestialObject.js
@@ -1,22 +1,54 @@
 
 
 class CelestialObject {
+    /**
+     * Default shape of the object. It contains a Array containing the shape-clusters of the same color.
+     * Each cluster is made by a color and a Array with Array(s) of points denoting the vertices of each shape.
+     * This allows to have multiple shapes of the same color on the same cluster.
+     * 
+     * [
+     * 
+     *      {
+     *          color: "",
+     *          shape: [
+     *              [Point, Point...],
+     *              ...
+     *          ]
+     *      },
+     *      ...
+     * ];
+     */
     static DEFAULT_SHAPE = [
         {
             color: "",
-            shape: [
-
-            ]
+            shape: []
         }
     ];
 
-    constructor(pos, v, mass=1) {
-        this.pos = pos;
+    constructor(pos, v, mass=1, angle=0) {
+        this._pos = pos;
 
-        this.mass = mass;
+        this._mass = mass;
+        this._angle = angle;
 
-        this.v = v;
-        this.a = new Point(0, 0);
+        this._v = v;
+        this._a = new Point(0, 0);
+    }
+
+    // GETTERS
+
+    /**
+     * Position of the object
+     */
+    get pos() {
+        return this._pos;
+    }
+
+    /**
+     * Mass of the object
+     */
+    get mass() {
+        return this._mass;
     }
 
     /**
@@ -27,19 +59,13 @@ class CelestialObject {
         this.a.advanceWithDirection(force.times(1 / this.mass));
     }
 
+    /**
+     * Updates the position and the shape of the player
+     */
     update() {
         this.v.advanceWithDirection(this.a);
         this.pos.advanceWithDirection(this.v);
 
         this.a.moveTo(0, 0); // Empty acceleration
-    }
-
-}
-
-class OrientableCelestialObject extends CelestialObject {
-    constructor(pos, angle, v, mass=1) {
-        super(pos, v, mass);
-
-        this.angle = angle;
     }
 }
