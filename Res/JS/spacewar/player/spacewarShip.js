9e53a7ad2de39655720690de6d45a37fc0487c0a
jkutkut <jorgeregonzalez@gmail.com>
2021-10-29 09:03:29 +0200

    ready to roll

--- Res/JS/spacewar/player/spacewarShip.js
+++ Res/JS/spacewar/player/spacewarShip.js
@@ -40,110 +40,21 @@ class SpacewarShip extends CelestialObject{
         }
     ];
 
+    static thrusterForce = 0.002;
+
     constructor(pos, v) {
         super(pos, v);
     }
 
-    get pos() {
-        return this._pos;
-    }
-
-    get shape() {
-        return this._shapeOBJ;
-    }
-
-    get exhaust() {
-        return this._exhaustOBJ;
-    }
-
-    get mass() {
-        return 1;
-    }
-
-    pushForward() {
+    thrusterOn() {
         this.exhaustOn = true;
 
-        let mF = 0.002;
-        let f = new Point(0, mF);
+        let f = new Point(0, this.thrusterForce);
         f.rotate(this.angle);
         this.applyForce(f);
     }
 
-    endPush() {
+    thrusterOff() {
         this.exhaustOn = false;
-        this._exhaustOBJ = [];
-    }
-
-    /**
-     * Applies the given force to the ship.
-     * @param {Point} force - Vector with the force applied to the ship.
-     * 
-     * @see ships doesn't have mass yet. Therefore, the force is technically an acceleration.
-     */
-    applyForce(force) {
-        this.a.advanceWithDirection(force);
-    }
-
-    update() {
-        this.v.advanceWithDirection(this.a);
-        this.pos.advanceWithDirection(this.v);
-
-        this.a.moveTo(0, 0); // Empty acceleration
-
-        this._shapeOBJ = {
-            shapes:[],
-            lines: [],
-            arcs: []
-        };
-        
-        
-        // Update shapes
-        for (let i = 0, j, l; i < SpacewarShip.DEFAULT_SHAPE.shapes.length; i++) {
-            this._shapeOBJ.shapes.push([]);
-
-            l = SpacewarShip.DEFAULT_SHAPE.shapes[i].length;
-            for (j = 0; j < l; j++) {
-                this._shapeOBJ.shapes[i].push(SpacewarShip.DEFAULT_SHAPE.shapes[i][j].clone()),
-                this._shapeOBJ.shapes[i][j].rotateBy(this.angle);
-                this._shapeOBJ.shapes[i][j].advanceWithDirection(this.pos);
-            }
-        }
-
-        // Update lines
-        for (let i = 0, j; i < SpacewarShip.DEFAULT_SHAPE.lines.length; i++) {
-            this._shapeOBJ.lines.push([]);
-
-            for (j = 0; j < 2; j++) {
-                this._shapeOBJ.lines[i][j] = SpacewarShip.DEFAULT_SHAPE.lines[i][j].clone(),
-                this._shapeOBJ.lines[i][j].rotateBy(this.angle);
-                this._shapeOBJ.lines[i][j].advanceWithDirection(this.pos);
-            }
-        }
-
-        // Update arcs
-        for (let i = 0, k; i < SpacewarShip.DEFAULT_SHAPE.arcs.length; i++) {
-            this._shapeOBJ.arcs.push([...SpacewarShip.DEFAULT_SHAPE.arcs[i]]);
-            for (k = 0; k < 2; k++) {
-                this._shapeOBJ.arcs[i][k] = this._shapeOBJ.arcs[i][k] + this.pos.pos[k];
-            }
-        }
-
-        if (this.exhaustOn) {
-            this._exhaustOBJ = [];
-
-            for (let i = 0; i < SpacewarShip.EXHAUST.length; i++) {
-                this._exhaustOBJ.push({shapes: [], lines: [], arcs: []});
-
-                for (let j = 0; j < SpacewarShip.EXHAUST[i].shapes.length; j++) {
-                    this._exhaustOBJ[i].shapes.push([]);
-
-                    for (let k = 0; k < SpacewarShip.EXHAUST[i].shapes[j].length; k++) {
-                        this._exhaustOBJ[i].shapes[j].push(SpacewarShip.EXHAUST[i].shapes[j][k].clone());
-                        this._exhaustOBJ[i].shapes[j][k].rotateBy(this.angle);
-                        this._exhaustOBJ[i].shapes[j][k].advanceWithDirection(this.pos);
-                    }
-                }
-            }
-        }
     }
 }
