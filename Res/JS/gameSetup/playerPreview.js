0e12b0c213ae32bcb4558518331020440848928b
jkutkut <jorgeregonzalez@gmail.com>
2021-11-15 09:15:10 +0100

    Logic to update the canvas moved to function. This code will be used to update the screen when updating the color of the objects

--- Res/JS/gameSetup/playerPreview.js
+++ Res/JS/gameSetup/playerPreview.js
@@ -4,7 +4,7 @@ bullets = [];
 ctxs = [];
 
 // When the page is loaded
-$(document).ready(function() {
+$(document).ready(() => {
     // When the user enters a name, verify it
     // for (let i = 1; i <= 4; i++) {
     //     $(`#p${i}Name`).on('input', () => {
@@ -13,33 +13,39 @@ $(document).ready(function() {
     //     });
     // }
   
-    let p = new Point(100, 75);
-    let v = new Point(0, 0);
-    for (let i = 0, j; i < 4; i++) {
-        ctxs[i] = document.getElementById(`p${i+1}Canvas`).getContext('2d');
-
-        ships[i] = new SpacewarShip(p.clone(), v.clone());
-        ships[i].update();
-        bullets[i] = new FastBullet(ships[i]);
-
-        bullets[i]._pos.advance(100, 0);
-        bullets[i].update();
-    }
-
-    for (i = 0; i < ships.length; i++) {
-        // Draw ship's full body
-        for (j = 0; j < ships[i].shape.length; j++) {
-            canvas_draw.subElement(ships[i].shape[j], ctxs[i]);
-        }
-    }
-
-    // Show bullets
-    for (i = 0; i < bullets.length; i++) {
-        for (j = 0; j < bullets[i].shape.length; j++) {
-            canvas_draw.subElement(bullets[i].shape[j], ctxs[i]);
-        }      
+    for (let i = 0; i < 4; i++) {
+        ctxs[i] = document.getElementById(`p${i + 1}Canvas`).getContext('2d');
+        updatePlayerScreen(i);
     }
 });
+
+/**
+ * Updates the screen of the given player
+ * @param {number} index - Index of the player (0 based)
+ */
+function updatePlayerScreen(i) {
+
+    let p = new Point(100, 75);
+    let v = new Point(0, 0);
+    
+    ships[i] = new SpacewarShip(p.clone(), v.clone());
+    bullets[i] = new FastBullet(ships[i]);
+
+    // update objects to be able to show them on the screen
+    ships[i].update();
+    bullets[i]._pos.advance(100, 0);
+    bullets[i].update();
+
+    // Draw ship's full body
+    for (j = 0; j < ships[i].shape.length; j++) {
+        canvas_draw.subElement(ships[i].shape[j], ctxs[i]);
+    }
+
+    // Show bullet
+    for (j = 0; j < bullets[i].shape.length; j++) {
+        canvas_draw.subElement(bullets[i].shape[j], ctxs[i]);
+    }
+}
 
 
 // function verifyName(index) {
