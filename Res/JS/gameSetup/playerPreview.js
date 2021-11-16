bef13656bb68da5b17bce530a1323e0e54bd95aa
jkutkut <jorgeregonzalez@gmail.com>
2021-11-16 07:48:25 +0100

    ships and bullets color can no be changed using the color pickers

--- Res/JS/gameSetup/playerPreview.js
+++ Res/JS/gameSetup/playerPreview.js
@@ -24,22 +24,44 @@ $(document).ready(() => {
  * @param {number} index - Index of the player (0 based)
  */
 function updatePlayerScreen(i) {
+    updateShip(i);
+    updateBullet(i);
+}
 
+function updateShip(i) {
     let p = new Point(100, 75);
     let v = new Point(0, 0);
     
-    ships[i] = new SpacewarShip(p.clone(), v.clone());
-    bullets[i] = new FastBullet(ships[i]);
+    ships[i] = new SpacewarShip(p, v);
 
-    // update objects to be able to show them on the screen
+    let c = $(`#p${i+1}ShipColor`).attr("data-current-color");
+    c = (c)? c : 'rgb(255, 255, 255)';
+
+    ships[i].bodyColor = c; // set the color of the ship to the one on the menu
+    
+    // update object to be able to show them on the screen
     ships[i].update();
-    bullets[i]._pos.advance(100, 0);
-    bullets[i].update();
+    
+    ctxs[i].fillStyle = Spacewar.COLOR.BG;
+    canvas_draw.arc(...p.pos, S * 2, 0, 2 * Math.PI, true, ctxs[i]);
 
     // Draw ship's full body
     for (j = 0; j < ships[i].shape.length; j++) {
         canvas_draw.subElement(ships[i].shape[j], ctxs[i]);
     }
+}
+
+function updateBullet(i) {
+    bullets[i] = new FastBullet(ships[i]);
+
+    let c = $(`#p${i+1}BulletColor`).attr("data-current-color");
+    c = (c)? c : 'rgb(255, 255, 255)';
+
+    bullets[i].bodyColor = c; // set the color of the ship to the one on the menu
+
+    // update object to be able to show them on the screen
+    bullets[i]._pos.advance(100, 0);
+    bullets[i].update();
 
     // Show bullet
     for (j = 0; j < bullets[i].shape.length; j++) {
